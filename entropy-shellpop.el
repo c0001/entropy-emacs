96fd562e954c0ae93e2715dc9bbf58ccca928839
Entropy <bmsac0001@gmail.com>
2019-08-30 17:26:21 +0800

    Unwind protect for prevent error occasion

--- entropy-shellpop.el
+++ entropy-shellpop.el
@@ -94,18 +94,29 @@
   (get-buffer-window buffer-name))
 
 (defun entropy/shellpop--prune-type-register-core (shellpop-type-register)
-  (let ((type-name (car shellpop-type-register))
-        (type-indexs (plist-get (cdr shellpop-type-register) :indexs))
-        ret)
-    (dolist (index type-indexs)
-      (let ((buffer-obj (entropy/shellpop--get-type-buffer-obj type-name (car index))))
-        (when (plist-get buffer-obj :isnew)
-          (push index ret))))
-    (dolist (index ret)
-      (setq type-indexs (delete index type-indexs)))
-    (setf (cdr shellpop-type-register)
-          (plist-put (cdr shellpop-type-register)
-                     :indexs type-indexs))))
+  (let* ((type-name (car shellpop-type-register))
+         (type-plist (cdr shellpop-type-register))
+         (type-indexs (plist-get type-plist :indexs))
+         (type-pointer (plist-get type-plist :pointer))
+         (type-pointer-alivep (if (integerp type-pointer)
+                                  (not (plist-get (entropy/shellpop--get-type-buffer-obj
+                                                   type-name type-pointer)
+                                                  :isnew))
+                                nil))
+         ret)
+    ;; pointer reset
+    (unless type-pointer-alivep
+      (setf type-plist (plist-put type-plist :pointer nil)))
+    ;; indexs reset
+    (when (not (null type-indexs))
+      (dolist (index type-indexs)
+        (let ((buffer-obj (entropy/shellpop--get-type-buffer-obj type-name (car index))))
+          (when (plist-get buffer-obj :isnew)
+            (push index ret))))
+      (dolist (index ret)
+        (setq type-indexs (delete index type-indexs)))
+      (setf type-plist
+            (plist-put type-plist :indexs type-indexs)))))
 
 (defun entropy/shellpop--prune-type-register ()
   (dolist (shellpop-type-register entropy/shellpop-type-register)
@@ -122,17 +133,17 @@
   (let* ((type-name shellpop-type-name)
          (shellpop-type-register (assoc type-name entropy/shellpop-type-register))
          (cur-type-plist (cdr shellpop-type-register))
-         (cur-type-indexs (plist-get cur-type-plist :indexs)))
-    (when (not (null cur-type-indexs))
-      (entropy/shellpop--prune-type-register-core shellpop-type-register))
+         (cur-type-indexs (plist-get cur-type-plist :indexs))
+         (cur-type-pointer (plist-get cur-type-plist :pointer)))
     (unless (entropy/shellpop--type-index-member buff-index cur-type-indexs)
       (setf cur-type-plist
             (plist-put cur-type-plist :indexs
                        (append (list (cons buff-index (read-string "Type slot 'DES': ")))
                                cur-type-indexs))))
-    (setf cur-type-plist
-          (plist-put cur-type-plist
-                     :pointer buff-index))))
+    (unless (eq cur-type-pointer buff-index)
+      (setf cur-type-plist
+            (plist-put cur-type-plist
+                       :pointer buff-index)))))
 
 (defun entropy/shellpop--make-prompt (shellpop-type-register-index)
   (let* ((name-list (entropy/cl-make-name-alist
@@ -165,19 +176,29 @@
                (buff-activep (plist-get buffer-ob :activep))
                (buff-isnew (plist-get buffer-ob :isnew))
                (buff-index (plist-get buffer-ob :index))
-               (buff (get-buffer-create buffn)))
-          (entropy/shellpop--put-index ,type-name buff-index)
-          (if buff-activep
-              (delete-window (get-buffer-window buffn))
-            (display-buffer buff)
-            (when buff-isnew
-              ,@type-body
-              (unless (equal buffn (buffer-name))
-                (kill-buffer buff)
-                (rename-buffer buffn))))))
+               (buff (get-buffer-create buffn))
+               (old-type-register (copy-tree (assoc ,type-name entropy/shellpop-type-register)))
+               unwind-trigger)
+          (unwind-protect
+              (progn 
+                (entropy/shellpop--put-index ,type-name buff-index)
+                (if buff-activep
+                    (delete-window (get-buffer-window buffn))
+                  (display-buffer buff)
+                  (when buff-isnew
+                    ,@type-body
+                    (unless (equal buffn (buffer-name))
+                      (kill-buffer buff)
+                      (rename-buffer buffn))))
+                (setq unwind-trigger t))
+            (unless unwind-trigger
+              (setf (alist-get ,type-name entropy/shellpop-type-register)
+                    (cdr old-type-register))
+              (kill-buffer buff)))))
 
      `(defun ,(intern func-name) (prompt)
         (interactive "P")
+        (entropy/shellpop--prune-type-register)
         (let* ((type-reg (assoc ,type-name entropy/shellpop-type-register))
                (type-plist (cdr type-reg))
                (type-pointer (plist-get type-plist :pointer))
