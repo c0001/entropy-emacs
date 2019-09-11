b5953a47664c2f73014c68f4db50e86e882e1478
Entropy <bmsac0001@gmail.com>
2019-09-11 09:09:09 +0800

    Optimize commentray declaration

--- entropy-cn-dict.el
+++ entropy-cn-dict.el
@@ -1,13 +1,38 @@
-;;; File name: entropy-cn-dict.el ---> for entropy-emacs
+;;; entropy-cn-dict.el --- Simple chinese dictionary using BAIDU API
 ;;
-;; Copyright (c) 2018 Entropy
-;;
-;; Author: Entropy
-;;
-;; This file is not part of GNU Emacs.
-;;
-;;; License: GPLv3
-
+;; * Copyright (C) date  author
+;; #+BEGIN_EXAMPLE
+;; Author:        Entropy <bmsac0001@gmail.com>
+;; Maintainer:    Entropy <bmsac001@gmail.com>
+;; URL:           https://github.com/c0001/entropy-cn-dict
+;; Package-Version: 0.1.0
+;; Created:       2018
+;; Keywords:      dictionary, translation, cn-dict
+;; Compatibility: GNU Emacs 24;
+;; Package-Requires: ((emacs "24") (cl-lib "0.5") (eww))
+;; 
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;; 
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; #+END_EXAMPLE
+;; 
+;; * Commentary:
+;; 
+;; A simple chinese dictionary using BAIDU API. 
+;; 
+;; * Configuration:
+;; 
+;; Added to load-path and requiring 
+;; 
 ;; * Code:
 
 (require 'eww)
