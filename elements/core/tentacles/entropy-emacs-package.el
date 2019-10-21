60ef60458742dc5bb712471ca29b2796e1f8a831
Entropy <bmsac0001@gmail.com>
2019-10-21 18:22:14 +0800

    Fix bug of previous commit.

--- elements/core/tentacles/entropy-emacs-package.el
+++ elements/core/tentacles/entropy-emacs-package.el
@@ -117,7 +117,8 @@
 (defvar entropy/emacs-package-prepare-done nil)
 
 (defun entropy/emacs-package-prepare-foras (&optional force)
-  (unless entropy/emacs-package-prepare-done
+  (unless (or entropy/emacs-package-prepare-done
+              (null force))
     (entropy/emacs-set-package-user-dir)
     (entropy/emacs-package--initial-package-archive)
     (entropy/emacs-package--refresh-gnupg-homedir)
