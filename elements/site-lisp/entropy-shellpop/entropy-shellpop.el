b0244947d3f7495f4569cf0da2b1c3ef2080d309
Entropy <bmsac0001@gmail.com>
2019-11-13 15:14:37 +0800

    `entropy-shellpop` first release came out

--- elements/site-lisp/entropy-shellpop/entropy-shellpop.el
+++ elements/site-lisp/entropy-shellpop/entropy-shellpop.el
@@ -4,17 +4,17 @@
 ;; #+BEGIN_EXAMPLE
 ;; Author:        Entropy <bmsac0001@gmail.com>
 ;; Maintainer:    Entropy <bmsac0001@gmail.com>
-;; 
+;;
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-;; 
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;; #+END_EXAMPLE
@@ -24,8 +24,8 @@
 ;; Created:       2019-08-29
 ;; Keywords:      shell-pop, shell
 ;; Compatibility: GNU Emacs emacs-version 26.1;
-;; Package-Requires: ((cl-lib "1.0") (shackle "1.0.3"))
-;; 
+;; Package-Requires: ((cl-lib "1.0") (shackle "1.0.3") (entropy-common-library "0.1.3"))
+;;
 ;;; Commentary:
 ;;
 ;; The shell popuped feature provision what seems like the =vscode
@@ -61,9 +61,131 @@
 ;;
 ;; You may customize variable =entropy/shellpop-pop-types= for more
 ;; specification, see its doc-string for more.
-;; 
+;;
+;;; Development
+
+;; For PR and extented aiming for, =entropy-shellpop= provide its own
+;; code context map, a illustration for thus as below sections:
+
+;;;; =shellpop-type-register= data structure
+
+;; This is the global host var-type designed for 'entropy-shellpop' to
+;; get the overview for all shellpop buffers in current emacs session of
+;; arbitrary shell-type (yes =entropy-shellpop= was multi-shell coexist
+;; possibly). It group the shellpop buffers into two-step tree, the top
+;; was referred to the =shell-type-name= and the subtree was for index of
+;; those buffers, that:
+
+;; #+BEGIN_EXAMPLE
+;;                             =========================================
+;;                                shellpop type register  illustration
+;;                             =========================================
+
+
+;;                                    +--------------------------+
+;;                                    |  shellpop-type-register  |
+;;                                    +-------------+------------+
+;;                                ---------/        |        \---------
+;;              eshell  ---------/             ansi | term             \---------  shell
+;;          +----------/--+                  +------v------+                  +--\----------+
+;;          | shelltype 1 |                  | shelltype 1 |                  | shelltype 1 |
+;;          +------+------+                  +------+------+                  +------+------+
+;;                 |                                |                                |
+;;       +---------+---------+            +---------+---------+            +---------+---------+
+;;       |         |         |            |         |         |            |         |         |
+;;       v         v         v            v         v         v            v         v         v
+;;   buffer 0   buffer 1  buffer 2    buffer 0   buffer 1  buffer 2    buffer 0   buffer 1  buffer 2
+;;       ^                                          ^                                          ^
+;;       |                                          |                                          |
+;;       |                                          |                                          |
+;;    pointer                                    pointer                                    pointer
+
+;; #+END_EXAMPLE
+
+;; With the illustration shown above, =shellpop-type-register= also has
+;; the *pointer* to indicate which shellpop buffer of specific shell-type
+;; are used currently, this *pointer* was used to briefly popping out the
+;; shellpop buffer without items chosen operation.
+
+;; Var =entropy/shellpop--type-register= was one implementation instance
+;; used for thus, its a alist which key was the =shellpop-type-name= a
+;; string, and the cdr was the plist whose slots are:
+
+;; + =:type-func=
+
+;;   This slot hosts the one plist to show the current shell-type caller
+;;   and its core function.
+
+;;   1) =:core=
+
+;;      Each shellpop-type has one underline function mainly for showing
+;;      the shellpop buffer according to this shell-type
+;;      specification. This slot was on that.
+
+;;   2) =:interact=
+
+;;      The main caller for this shellpop-type, a function with
+;;      interacive features.
+
+;; + =:indexs=
+
+;;   Alist to represent the current shellpop-type shellpop buffers,
+;;   formed as
+;;   : '((0  . tag-name_0) (1 . tag-name_1) (2 . tag-name_2))'
+
+;;   *tag-name* was the human readable description for its shellpop
+;;   buffer.
+
+;; + =:pointer=
+
+;;   *Interger* to indicate which shellpop buffer of this shellpop-type
+;;   is used currently.
+
+;;;; =shellpop-buffer-object= data structure
+
+;; As that metioned for section for *shellpop-type-register*, each
+;; shellpop buffer of one shellpop-type was mapped in one indexed alist,
+;; thus for term, the index space can be unlimited restricted by the
+;; emacs's threshold for integer. Says in the other hand, in imagination
+;; case, there's infinite shellpop buffers exists in the same time,
+;; exists or inexists as the top level concept.
+
+;; Each shellpop buffer registed in =entropy/shellpop--type-register= was
+;; a index with its description, but without any status recorded, and for
+;; that the register were not background upate in real-time as a service
+;; running for watching as 'watch-dog'. For thus, =entropy-sdcv= gives a
+;; shellpop buffer status checking probe function
+;; ~entropy/shellpop--get-type-buffer-obj~.
+
+;; The probe function recieve two arguments i.e. the =shellpop-type-name=
+;; and the buffer-index (ps: optionally), return one plist strucured of
+;; =shellpop-buffer-object= struct. This data structure stored the
+;; various buffer status information, include:
+
+;; 1) Whether this index of buffer was exists, key =:isnew= indicated
+;;    that.
+;; 2) Whether the indexed exists buffer shown in the current frame, key
+;;    =:activep= indicated that.
+;; 3) The index of this shellpop buffer, key =:index= indicated that.
+;; 4) The buffer-naem of the indexed shellpop buffer, key =:buffer-name=
+;;    indicated that.
+
+;;;; Extensible developing
+
+;; As for compability case thought for, I think the
+;; =shellpop-type-register= and =shellpop-buffer-object= provision was
+;; enoughly for you to write some tools to enhance the shellpop behavior,
+;; and given the rich way to experience thus. Just do with your flying
+;; mind.
+
+;;; Changelog
+
+;; - [2019-11-13] *v0.1.0* release out.
+
+;;   The first release of =entropy-sdcv=
+
 ;;; Code:
-;; 
+;;
 ;;;; require
 (require 'cl-lib)
 (require 'shackle)
@@ -71,7 +193,8 @@
 
 ;;;; defcustom
 (defgroup entropy/shellpop-customized-group nil
-  "entropy-shellpop customized variable group.")
+  "entropy-shellpop customized variable group."
+  :group 'convenience)
 
 (defcustom entropy/shellpop-eshell-popup-key
   (lambda (func) (global-set-key (kbd "<f9>") func))
@@ -136,7 +259,7 @@ Slot description:
    The forms to enable shell entity, for example: '(eshell)' or
    '(ansi-term \"/bin/bash\")'.
 "
-  
+
   :type 'sexp
   :group 'entropy/shellpop-customized-group)
 
@@ -322,7 +445,7 @@ shellpop type")
       (push buffn closed))
     closed))
 
-;;;;; prunning registered shellpop type entity 
+;;;;; prunning registered shellpop type entity
 (defun entropy/shellpop--prune-type-register-core (shellpop-type-register)
   (let* ((type-name (car shellpop-type-register))
          (type-plist (cdr shellpop-type-register))
@@ -339,7 +462,7 @@ shellpop type")
     (unless type-pointer-alivep
       (setq type-plist (plist-put type-plist :pointer nil))
       (setf (cdr shellpop-type-register) type-plist))
-    
+
     ;; indexs reset
     (when (not (null type-indexs))
       (dolist (index type-indexs)
