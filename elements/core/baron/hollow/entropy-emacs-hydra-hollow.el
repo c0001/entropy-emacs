c0861617b82d442bca98928f515859c9b4e11c6d
Entropy <bmsac0001@gmail.com>
2020-02-14 20:01:17 +0800

    Add 'global' and 'map inject' indicator to pretty-hydra show

--- elements/core/baron/hollow/entropy-emacs-hydra-hollow.el
+++ elements/core/baron/hollow/entropy-emacs-hydra-hollow.el
@@ -10,7 +10,7 @@
 
 ;; ** libraries
 (defun entropy/emacs-hydra-hollow--gets-pretty-hydra-heads-keybind
-    (pretty-heads-groups)
+    (pretty-heads-group)
   (let (group-extract-func
         group-element-parse-func
         return-var)
@@ -31,11 +31,25 @@
                            (not (stringp el)))
                   (push el rtn)))
               rtn)))
-    (dolist (el (funcall group-extract-func pretty-heads-groups))
+    (dolist (el (funcall group-extract-func pretty-heads-group))
       (dolist (elt el)
         (push (funcall group-element-parse-func elt)
               return-var)))
     return-var))
+
+(defun entropy/emacs-hydra-hollow--split-pretty-hydra-group-heads
+    (pretty-heads-group)
+  (let (split-heads)
+    (cl-loop for cnt from 0 to (- (/ (length pretty-heads-group) 2) 1)
+             do (let* ((cnt (* cnt 2))
+                       (group (nth cnt pretty-heads-group))
+                       (heads (nth (+ cnt 1) pretty-heads-group))
+                       rtn)
+                  (dolist (el heads)
+                    (push (list group (list el)) rtn))
+                  (when rtn
+                    (setq split-heads (append split-heads (reverse rtn))))))
+    split-heads))
 
 (defun entropy/emacs-hydra-hollow--merge-pretty-hydra-sparse-heads
     (pretty-heads-list)
@@ -51,11 +65,11 @@
                    when (equal (car (nth pt rest)) group)
                    do (push pt pointer-list))
           (if (null pointer-list)
-              (push (car manipulate-list) pretty-heads-group-fake)
+              (push (pop manipulate-list) pretty-heads-group-fake)
             (progn
              (dolist (el pointer-list)
-               (push (cadr (nth el rest)) collect))
-             (push (cadar manipulate-list) collect))
+               (push (caadr (nth el rest)) collect))
+             (push (caadar manipulate-list) collect))
             (when collect
               (push (list group collect) pretty-heads-group-fake))
             (progn
@@ -64,11 +78,9 @@
                  (+ pt 1) nil manipulate-list))
               (pop manipulate-list)
               (setq manipulate-list
-                    (delete* nil manipulate-list)))))))
-    (dolist (el pretty-heads-group-fake)
-      (if (listp (caadr el))
-          (setq rtn (append rtn el))
-        (setq rtn (append rtn (list (car el) (cdr el))))))
+                    (cl-delete nil manipulate-list)))))))
+    (dolist (el (reverse pretty-heads-group-fake))
+      (setq rtn (append rtn el)))
     rtn))
 
 ;; ** apis
@@ -109,42 +121,77 @@
 (cl-defmacro entropy/emacs-hydra-hollow-add-for-top-dispatch
     (group &key notation key command toggle exit global-bind)
   (declare (indent 1))
-  `(progn
-     (entropy/emacs-hydra-hollow-init-top-dispatch)
-     (add-to-list
-      'entropy/emacs-hydra-hollow-top-dispatch-register
-      '(,group
-        ((,key ,command ,notation
-               :toggle ,toggle :exit ,exit
-               :face entropy/emacs-defface-face-for-hydra-heads-orange-face))))
-     (pretty-hydra-define+ entropy/emacs-hydra-hollow-top-dispatch
-       nil
-       (,group
-        ((,key ,command ,notation
-               :toggle ,toggle :exit ,exit
-               :face entropy/emacs-defface-face-for-hydra-heads-orange-face))))
-     (when ,global-bind
-       (global-set-key (kbd ,key) #',command))))
+  (let ((notation
+         (if global-bind
+             (format "%s (g)" notation)
+           notation)))
+    `(progn
+       (entropy/emacs-hydra-hollow-init-top-dispatch)
+       (add-to-list
+        'entropy/emacs-hydra-hollow-top-dispatch-register
+        '(,group
+          ((,key ,command ,notation
+                 :toggle ,toggle :exit ,exit
+                 :face entropy/emacs-defface-face-for-hydra-heads-orange-face))))
+       (pretty-hydra-define+ entropy/emacs-hydra-hollow-top-dispatch
+         nil
+         (,group
+          ((,key ,command ,notation
+                 :toggle ,toggle :exit ,exit
+                 :face entropy/emacs-defface-face-for-hydra-heads-orange-face))))
+       (when ,global-bind
+         (global-set-key (kbd ,key) #',command)))))
 
 ;; *** majro mode dispacher
 
 (cl-defmacro entropy/emacs-hydra-hollow-define-major-mode-hydra
     (mode feature mode-map body heads-plist)
-  `(let ()
-     (major-mode-hydra-define ,mode
-       ,body
-       ,heads-plist)
-     (entropy/emacs-lazy-load-simple ',feature
-       (let ((binds (entropy/emacs-hydra-hollow--gets-pretty-hydra-heads-keybind
-                     ',heads-plist)))
-         (dolist (el binds)
-           (let ((command (plist-get el :command))
-                 (key (plist-get el :key))
-                 (map-inject (plist-get el :map-inject)))
-             (when map-inject
-               (message "Binding key '%s' of command '%s' to map '%s' ..."
-                        key (symbol-name command) (symbol-name ',mode-map))
-               (define-key ,mode-map (kbd key) command))))))))
+  (let ((split-heads (copy-tree
+                      (entropy/emacs-hydra-hollow--split-pretty-hydra-group-heads
+                       heads-plist)))
+        patched-heads-group)
+    
+    (dolist (head split-heads)
+      (let* ((head-plist (caadr head))
+             (head-attr (cdddr head-plist))
+             (map-inject (plist-get head-attr :map-inject))
+             (notation (caddr head-plist)))
+        (push 
+         (list
+          (car head)
+          (list
+           (append
+            (list
+             (car head-plist)
+             (cadr head-plist)
+             (if map-inject
+                 (format "%s (m)" notation)
+               notation))
+            (cdddr head-plist))))
+         patched-heads-group)))
+
+    (setq patched-heads-group (reverse patched-heads-group)
+          patched-heads-group
+          (entropy/emacs-hydra-hollow--merge-pretty-hydra-sparse-heads
+           patched-heads-group))           
+
+    `(let ()
+       (entropy/emacs-lazy-load-simple ',feature
+         (let ((binds (entropy/emacs-hydra-hollow--gets-pretty-hydra-heads-keybind
+                       ',heads-plist)))
+
+           (major-mode-hydra-define ,mode
+             ,body
+             ,patched-heads-group)              
+           
+           (dolist (el binds)
+             (let ((command (plist-get el :command))
+                   (key (plist-get el :key))
+                   (map-inject (plist-get el :map-inject)))
+               (when map-inject
+                 (message "Binding key '%s' of command '%s' to map '%s' ..."
+                          key (symbol-name command) (symbol-name ',mode-map))
+                 (define-key ,mode-map (kbd key) command)))))))))
 
 (cl-defmacro entropy/emacs-hydra-hollow-add-to-major-mode-hydra
     (mode mode-map heads-plist)
