f9923b5eba886e510ac4dbb9778022ac1a04923b
Entropy <bmsac0001@gmail.com>
2020-04-24 18:02:45 +0800

    Squashed 'elements/site-lisp/liberime/' changes from 9e05c6f..9c314b9
    9c314b9 msys2-build.sh: remove strip
    dcc4c57 Fix msys2-build.sh
    6b7fd57 Fix .travis.yml again
    b476d41 Fix .travis.yml
    6dd9510 Simplify README about msys2
    a75d160 msys2-build.sh: install liberime msys package
    82ec439 简化 msys2-build.sh, 因为 librime 已经进入 msys2 仓库了
    33847ad Update README
    e536d3c Merge pull request #55 from merrickluo/auto-build
    bd19ca7 documentation for liberime-auto-build
    db3f38d add a custom variable for auto build
    e081f4b Merge pull request #53 from merrickluo/show-comments
    cadd278 minor refactor to convert string to emacs
    7907b04 add comment as candidate text property
    40c03e6 format code with format-all-mode
    ccddac0 Update liberime-get/set-scheme-config's test #50
    e30f40e Remove some files
    9cf15fa liberime-select-schema-1: message "" after success
    88e0c41 test.el -> liberime-test.el
    fb4646c Improve liberime-message
    9903ef6 Move defvar to file begin
    557c24b liberime-select-schema-1: improve message
    0b311f8 liberime-set-page-size: default to 10 instead of 100
    325b01c liberime--start: run liberime-select-schema when liberime-current-schema non-nil
    6cba535 Add variable: liberime-current-schema
    adc6599 liberime-select-schema-1: ignore errors
    ec4ad67 Update README
    29a6318 traits.app_name = "rime.emacs-liberime", traits.distribution_code_name= "emacs-liberime"
    6cd5acd liberime-core: add composition_size, menu_size
    bde86df liberime-core:  *_a -> *_array
    b0068b8 Make liberime-select-schema more reliable
    edde870 Add liberime-get-status
    e0c5ee0 Merge pull request #51 from sjkyspa/master
    3e247dc Simplify README.org
    a989cfd Simplify README.org
    7bfb271 liberime-config.el:  -*- lexical-binding: t; -*-
    5873167 (declare-function w32-shell-execute "w32fns")
    b9a3a64 * liberime.el (liberime-select-schema-interactive): fix free schema variable
    eaa7a8e liberime.el: -*- lexical-binding: t; -*-
    076a931 fix: add bare lib folder in link path
    
    git-subtree-dir: elements/site-lisp/liberime
    git-subtree-split: 9c314b9efe7df6693e4ff97b2d880cf35ca3c9ae

--- liberime-test.el
+++ liberime-test.el
@@ -6,23 +6,26 @@
        ,@body
        (- (float-time) ,start))))
 
-(require 'liberime-core)
-;; darwin
-(setq rime-data-dir "/Library/Input Methods/Squirrel.app/Contents/SharedSupport")
-;; linux
-(setq rime-data-dir "/usr/share/rime-data")
-(require 'liberime-config)
+(let ((liberime-user-data-dir (locate-user-emacs-file "test/rime")))
+  (load "liberime.el"))
 
-(liberime-start rime-data-dir (expand-file-name "~/.emacs.d/test/rime"))
 (liberime-get-schema-list)
 (liberime-select-schema "luna_pinyin_simp")
 (liberime-search "wode" nil)
 (liberime-finalize)
 (liberime-get-user-config "default.custom" "patch/menu/page_size" "int")
 (liberime-set-user-config "default.custom" "patch/menu/page_size" 100 "int")
-(liberime-get-schema-config nil "speller/auto_select" "bool")
-(liberime-set-schema-config nil "speller/auto_select" true "bool")
+(liberime-get-schema-config "" "speller/auto_select" "bool")
+(liberime-set-schema-config "" "speller/auto_select" true "bool")
 (liberime-sync-user-data)
+
+(defun try-context()
+  (liberime-clear-composition)
+  (liberime-process-key (string-to-char "w"))
+  (liberime-process-key (string-to-char "o"))
+  (liberime-get-context))
+
+(try-context)
 
 (require 'pyim)
 (setq pyim-default-scheme 'quanpin)
