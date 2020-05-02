42dd6bb77a9660b717c98134df54b1f521ca67b6
Entropy <bmsac0001@gmail.com>
2020-05-02 17:21:13 +0800

    Dirty fixing the modeline selected window status bug after treemacs following

--- elements/core/tentacles/entropy-emacs-ivy.el
+++ elements/core/tentacles/entropy-emacs-ivy.el
@@ -412,7 +412,14 @@ this variable used to patching for origin `counsel-git'.")
     "
     (with-ivy-window
       (let ((default-directory entropy/emacs-ivy-counsel-git-root))
-        (find-file x)))))
+        (find-file x)
+        ;; prompt in echo area for foce refresh modeline selected
+        ;; window referrence status to prevent modeline status doesn't
+        ;; update in some occasions e.g. treemacs auto follow
+        ;; procedure.
+        (message "Opened vcs controlled file \"%s\" in git root \"%s\""
+                 (file-name-base x)
+                 entropy/emacs-ivy-counsel-git-root)))))
 
 ;; *** use counsel css for quickly search css selector
 (use-package counsel-css
