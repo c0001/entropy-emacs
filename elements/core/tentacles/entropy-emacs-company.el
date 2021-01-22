a0213469f4f6654432ec387fc510bfd510422dea
Entropy <bmsac0001@gmail.com>
2021-01-23 07:23:05 +0800

    Use `company-box-icons-images' defaulty
    All the icons for company box will occur memory leak scene while long
    time usage and poped out referred performances issue.

--- elements/core/tentacles/entropy-emacs-company.el
+++ elements/core/tentacles/entropy-emacs-company.el
@@ -429,7 +429,10 @@ NOTE: this function is an around advice wrapper."
         ;; FIXME: company box idle delay smaller than 0.42 will cause
         ;; first candi doc not show
         (max 0.42 entropy/emacs-company-quickhelp-delay-default)
-        company-box-show-single-candidate 'always)
+        company-box-show-single-candidate 'always
+        company-box-enable-icon t
+        company-box-icons-alist 'company-box-icons-images
+        company-box-icon-right-margin 1)
 
 ;; **** config
   :config
@@ -486,7 +489,7 @@ initial frame background face sets did by
               :around
               #'entropy/emacs-company--company-candis-length-follow-advice)
 
-;; ***** icons patch
+;; ***** Icons specified patch
 
   (with-no-warnings
     ;; Prettify icons
@@ -499,41 +502,10 @@ initial frame background face sets did by
                 ((boundp sym) 'Variable)
                 ((symbolp sym) 'Text)
                 (t . nil)))))
-    (advice-add #'company-box-icons--elisp :override #'entropy/emacs-company--company-box-icons-elisp))
+    (advice-add #'company-box-icons--elisp
+                :override
+                #'entropy/emacs-company--company-box-icons-elisp))
 
-  (progn
-    (declare-function all-the-icons-faicon 'all-the-icons)
-    (declare-function all-the-icons-material 'all-the-icons)
-    (declare-function all-the-icons-octicon 'all-the-icons)
-    (setq entropy/emacs-company--company-box-icons-all-the-icons
-          `((Unknown . ,(all-the-icons-material "find_in_page" :height 0.8 :v-adjust -0.15))
-            (Text . ,(all-the-icons-faicon "text-width" :height 0.8 :v-adjust -0.02))
-            (Method . ,(all-the-icons-faicon "cube" :height 0.8 :v-adjust -0.02 :face 'all-the-icons-purple))
-            (Function . ,(all-the-icons-faicon "cube" :height 0.8 :v-adjust -0.02 :face 'all-the-icons-purple))
-            (Constructor . ,(all-the-icons-faicon "cube" :height 0.8 :v-adjust -0.02 :face 'all-the-icons-purple))
-            (Field . ,(all-the-icons-octicon "tag" :height 0.85 :v-adjust 0 :face 'all-the-icons-lblue))
-            (Variable . ,(all-the-icons-octicon "tag" :height 0.85 :v-adjust 0 :face 'all-the-icons-lblue))
-            (Class . ,(all-the-icons-material "settings_input_component" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-orange))
-            (Interface . ,(all-the-icons-material "share" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-lblue))
-            (Module . ,(all-the-icons-material "view_module" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-lblue))
-            (Property . ,(all-the-icons-faicon "wrench" :height 0.8 :v-adjust -0.02))
-            (Unit . ,(all-the-icons-material "settings_system_daydream" :height 0.8 :v-adjust -0.15))
-            (Value . ,(all-the-icons-material "format_align_right" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-lblue))
-            (Enum . ,(all-the-icons-material "storage" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-orange))
-            (Keyword . ,(all-the-icons-material "filter_center_focus" :height 0.8 :v-adjust -0.15))
-            (Snippet . ,(all-the-icons-material "format_align_center" :height 0.8 :v-adjust -0.15))
-            (Color . ,(all-the-icons-material "palette" :height 0.8 :v-adjust -0.15))
-            (File . ,(all-the-icons-faicon "file-o" :height 0.8 :v-adjust -0.02))
-            (Reference . ,(all-the-icons-material "collections_bookmark" :height 0.8 :v-adjust -0.15))
-            (Folder . ,(all-the-icons-faicon "folder-open" :height 0.8 :v-adjust -0.02))
-            (EnumMember . ,(all-the-icons-material "format_align_right" :height 0.8 :v-adjust -0.15))
-            (Constant . ,(all-the-icons-faicon "square-o" :height 0.8 :v-adjust -0.1))
-            (Struct . ,(all-the-icons-material "settings_input_component" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-orange))
-            (Event . ,(all-the-icons-octicon "zap" :height 0.8 :v-adjust 0 :face 'all-the-icons-orange))
-            (Operator . ,(all-the-icons-material "control_point" :height 0.8 :v-adjust -0.15))
-            (TypeParameter . ,(all-the-icons-faicon "arrows" :height 0.8 :v-adjust -0.02))
-            (Template . ,(all-the-icons-material "format_align_left" :height 0.8 :v-adjust -0.15)))
-          company-box-icons-alist 'entropy/emacs-company--company-box-icons-all-the-icons))
   )
 
 ;; *** Better sorting and filtering
@@ -649,7 +621,7 @@ completion when calling: 'execute-extended-command' or
         (candi)
       "Common text icon view for non-matched candi of dev env so
 that for en-words candi recognized "
-      'Unknown)
+      'Text)
     (setq company-box-icons-functions
           (append company-box-icons-functions
                   '(entropy/emacs-company--company-en-words-icons-for-company-box)))
