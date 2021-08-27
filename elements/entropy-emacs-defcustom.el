2121d069d3604e100b4d04a9fc7f4bf2c0b48d81
Entropy <bmsac0001@gmail.com>
2021-08-27 14:36:12 +0800

    Reset `company-idle-delay' to short one since we've enhanced further

--- elements/entropy-emacs-defcustom.el
+++ elements/entropy-emacs-defcustom.el
@@ -1102,7 +1102,7 @@ rich enhanced purpose there are one another option:
           (const :tag "Rich-hence (company-box)" company-box))
   :group 'entropy/emacs-customize-group-for-company-mode)
 
-(defcustom entropy/emacs-company-idle-delay-default 1.5
+(defcustom entropy/emacs-company-idle-delay-default 0.5
   "Default eemacs specified set for `company-idle-delay`."
   :type 'number
   :group 'entropy/emacs-customize-group-for-company-mode)
