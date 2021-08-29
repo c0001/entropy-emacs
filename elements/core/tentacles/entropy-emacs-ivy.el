428a972d12fc2202e659439f2de3b2c5aa8aa9ab
Entropy <bmsac0001@gmail.com>
2021-08-29 15:50:41 +0800

    Archive obsolete schemas

--- elements/core/tentacles/entropy-emacs-ivy.el
+++ elements/core/tentacles/entropy-emacs-ivy.el
@@ -327,32 +327,7 @@ unwind occasion.")
   :config
   ;; make `swiper-all' restore origin window-configuration when unwind
   (advice-add 'swiper-all :around #'entropy/emacs-ivy--swiper-all-restore-wfg)
-
-  ;; ==assign format-function to swiper for fix some-bug==
-  ;; when using `all-the-icons-dired' with swiper, icons displayed in
-  ;; associate dired-buffer are mapped with all-the-icons spec fonts
-  ;; which can not rendered correctly for other fonts. ivy's defaut
-  ;; format function `ivy-format-function-default' used for `swiper'
-  ;; which using `identify' to format inactive candis which using the
-  ;; default face as font-lock atribtue, it will corrupts the
-  ;; correctly font displaying when set spec font to this default
-  ;; face.
-  (entropy/emacs-lazy-load-simple all-the-icons-dired
-    (defun entropy/emacs-ivy--swiper-format-function-for-dired (cands)
-      "Transform CANDS into a string for minibuffer."
-      (ivy--format-function-generic
-       (lambda (str)
-         (ivy--add-face str 'ivy-current-match))
-       (lambda (str)
-         (cond
-          ((eq major-mode 'dired-mode)
-           (ivy--add-face str 'entropy/emacs-defface-face-for-swiper-dired-candi-inactive-face))
-          (t
-           (identity str))))
-       cands
-       "\n"))
-    (add-to-list 'ivy-format-functions-alist
-                 (cons 'swiper #'entropy/emacs-ivy--swiper-format-function-for-dired))))
+  )
 
 ;; ** counsel
 (use-package counsel
--- elements/core/tentacles/entropy-emacs-tools.el
+++ elements/core/tentacles/entropy-emacs-tools.el
@@ -943,57 +943,6 @@ https://github.com/atykhonov/google-translate/issues/98#issuecomment-562870854
       "A simple and cool pomodoro technique timer"
       :enable t :exit t :global-bind t)))))
 
-;; *** maple preview
-(use-package maple-preview
-  :if (and
-       ;; disable defaulty since its obsolete almostly about 3 years
-       ;; since 2018, but still can be used to scratch studying.
-       nil
-       (equal entropy/emacs-ext-elpkg-get-type 'submodules-melpa-local))
-  :ensure nil
-  :commands (maple-preview-mode)
-  :init
-  (setq maple-preview:allow-modes '(org-mode markdown-mode gfm-mode html-mode web-mode))
-  :config
-  (defun entropy/emacs-tools-maple-preview:schema-auto-hooks ()
-    (dolist ($el '(windmove-left
-                   windmove-right
-                   windmove-up
-                   windmove-down))
-      (advice-add $el :after #'maple-preview:send-to-server))
-    (entropy/emacs-lazy-load-simple eyebrowse
-      (advice-add 'eyebrowse-switch-to-window-config
-                  :after
-                  #'maple-preview:send-to-server))
-    (advice-add 'other-window :after #'maple-preview:send-to-server)
-    (entropy/emacs-lazy-load-simple markdown-mode
-      (advice-add 'markdown-outdent-or-delete
-                  :after
-                  #'maple-preview:send-to-server))
-    (advice-add 'backward-delete-char-untabify
-                :after #'maple-preview:send-to-server))
-
-  (defun entropy/emacs-tools--maple-preview:schema-finialize-hooks ()
-    (dolist ($el '(windmove-left
-                   windmove-right
-                   windmove-up
-                   windmove-down))
-      (advice-remove $el #'maple-preview:send-to-server))
-    (when (fboundp 'eyebrowse-switch-to-window-config)
-      (advice-remove 'eyebrowse-switch-to-window-config
-                     #'maple-preview:send-to-server))
-    (advice-remove 'other-window #'maple-preview:send-to-server)
-    (when (fboundp 'markdown-outdent-or-delete)
-      (advice-remove 'markdown-outdent-or-delete
-                     #'maple-preview:send-to-server))
-    (advice-remove 'backward-delete-char-untabify
-                   #'maple-preview:send-to-server))
-
-  (add-hook 'maple-preview:auto-hook
-            #'entropy/emacs-tools-maple-preview:schema-auto-hooks)
-  (add-hook 'maple-preview:finialize-hook
-            #'entropy/emacs-tools--maple-preview:schema-finialize-hooks))
-
 ;; ** Misc
 ;; *** copy path, url, etc.
 (use-package copyit
