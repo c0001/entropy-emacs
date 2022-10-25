c0001/entropy-emacs

commit b1b5e9ec116080707f7f08feee9a85eb31a157d8

Sets of bugs fixed

elements/core/wasteland/func-binds/entropy-emacs-defun.el
@@ -798,27 +798,37 @@ c)'."
 
 ;; *** batch `eval-after-load'
 (defmacro entropy/emacs-eval-after-load (feature &rest body)
-  (let (forms (cnt 1) bound)
-    (cond ((symbolp feature)
+  "Wrap BODY into FEATURE using `eval-after-load'.
+
+Feature is can be a FILE arg of `eval-after-load' or a list of
+that (note that this is a macro that if feature is a list, it
+means its a form of a list)."
+  (let (forms
+        (extract-item
+         (lambda (file)
+           (if (stringp file)
+               file
+             `(quote ,file)))))
+    (cond ((not (listp feature))
            (setq forms
-                 `(eval-after-load ',feature
+                 `(eval-after-load ,(funcall extract-item feature)
+                    (lambda ()
+                      ,@body))))
+          ((and (listp feature)
+                (= 1 (length feature)))
+           (setq forms
+                 `(eval-after-load ,(funcall extract-item (car feature))
                     (lambda ()
                       ,@body))))
           ((and (listp feature)
                 (> (length feature) 1))
            (setq feature (reverse feature)
-                 forms `(eval-after-load ',(car feature) (lambda () ,@body))
-                 bound (length (cdr feature)))
-           (dolist (load (cdr feature))
+                 forms `(eval-after-load ,(funcall extract-item (car feature))
+                          (lambda () ,@body)))
+           (dolist (file (cdr feature))
              (setq forms
-                   `(eval-after-load ',load
-                      ,forms))))
-          ((and (listp feature)
-                (= 1 (length feature)))
-           (setq forms
-                 `(eval-after-load ',(car feature)
-                    (lambda ()
-                      ,@body)))))
+                   `(eval-after-load ,(funcall extract-item file)
+                      ,forms)))))
     forms))
 
 ;; *** hook refer
@@ -857,10 +867,8 @@ in case that file does not provide any feature."
   "Wrapping BODY to a function named with suffix by NAME into
 =entropy-emacs= startup trail hook.
 
-=entropy-emacs= trail hook are `entropy/emacs-init-mini-hook',
-`entropy/emacs-init-X-hook', `entropy/emacs-pdumper-load-hook' and
-`entropy/emacs-startup-end-hook', see their their doc-string for
-details.
+See `entropy/emacs-select-trail-hook' for details of what is
+=entropy-emacs= trail hook.
 
 BODY can be forms or a expanded FORM-PLIST (see
 `entropy/emacs-get-plist-form') in which case there's some keys on
@@ -909,26 +917,79 @@ functional aim to:
          (setq entropy/emacs-startup-end-hook
                (append entropy/emacs-startup-end-hook
                        '(,func))))
-        (entropy/emacs-fall-love-with-pdumper
-         (setq entropy/emacs-pdumper-load-hook
-               (append entropy/emacs-pdumper-load-hook
-                       '(,func))))
         (t
-         (set (entropy/emacs-select-x-hook)
-              (append (symbol-value (entropy/emacs-select-x-hook))
+         (set (entropy/emacs-select-trail-hook)
+              (append (symbol-value (entropy/emacs-select-trail-hook))
                       '(,func))))))))
 
 (defun entropy/emacs-lazy-initial-form
-    (list-var initial-func-suffix-name initial-var-suffix-name abbrev-name adder-name &rest form_args)
+    (list-var
+     initial-func-suffix-name initial-var-suffix-name
+     abbrev-name adder-name
+     &rest form_args)
+  "Generate a form whose functional is to wrap some forms into a
+GENED-FUNTION who named with ABBREV-NAME and INITIAL-SUFFIX-NAME
+(a non empty string) and add it with some *tricks* to
+=entropy-emacs= trial hook (see
+`entropy/emacs-lazy-with-load-trail') with *once calling* feature
+for =entropy-emacs= lazy-load meaning.
+
+The '&rest' type FORM-ARGS is orderd with ADDER-TYPE ADDER-FLAG
+and BODY.
+
+ADDER-TYPE is a symbol of either 'add-hook' or 'advice-add'.
+
+When ADDER-FLAG is non-nil, whatever value it hosted is meaning
+for a `advice-add's WHERE argument defination.
+
+There's two tricks:
+
+1. Directly add GENED-FUNCTION into the =entropy-emacs= trail hook
+   when `entropy/emacs-custom-enable-lazy-load' is non-nil.
+
+2. Using LIST-VAR and consider each element of it is a hook (when
+   the ADDER-FLAG is nil) or a function (when ADDER-TYPE is
+   non-nil), and add the GENED-FUNCTION into the hook or the
+   advice env respectively using a ADDER-FUNCION named with the
+   GENED-FUNCTION name suffixing on ADDER-NAME (a non empty
+   string) for insteadly add it into =entropy-emacs= trail hook to
+   get the lazy load effection.
+
+The GENED-FUNCTION has the '&rest' type argument
+'$_|internal-args' which has different meaning for different value
+of ADDER-TYPE, it's meaningless when ADDER-TYPE is `add-hook',
+otherwise it has the meaning for `add-function' remaining.
+
+The GENED-FUNCTION is invocated just once that's why it is used
+for lazy-loading. This functional based on the INITIAL-VAR which
+named with ABBREV-NAME and INITIAL-VAR-SUFFIX-NAME (a non empty
+string), BODY is wrapped into a form like:
+#+begin_src emacs-lisp
+  (unless INITIAL-VAR
+    ,@BODY
+    (setq INITIAL-VAR t))
+#+end_src
+
+At last, ABBREV-NAME is feature based name string so that other
+macro or function can use this function to generate the
+GENED-FUNCTION with their own name abbreviated."
+
   (let* ((func (intern (concat abbrev-name "_" initial-func-suffix-name)))
          (adder-func (intern (concat (symbol-name func) "_" adder-name)))
          (var (intern (concat abbrev-name "_+" initial-var-suffix-name)))
          (adder-type (car form_args))
          (adder-flag (cadr form_args))
          (func-body (nth 2 form_args)))
+    (unless (member adder-type '(add-hook advice-add))
+      (error "Wrong adder-type for lazy-initial form '%s'"
+             abbrev-name))
+    (when (and (eq adder-type 'add-hook)
+               (not (null adder-flag)))
+      (error "Non-nil flag for 'add-hook' adder-type for lazy-initial form '%s'"
+             abbrev-name))
     `(progn
        (defvar ,var nil)
-       (defun ,func (&rest _)
+       (defun ,func (&rest $_|internal-args)
          (let ((head-time (time-to-seconds))
                end-time)
            (unless ,var
@@ -949,13 +1010,12 @@ functional aim to:
               (cyan (format "%f" (- end-time head-time)))
               (green "seconds."))
              (redisplay t))))
-       (let ((hook (entropy/emacs-select-x-hook)))
+       (let ((hook (entropy/emacs-select-trail-hook)))
          (cond
-          ((and (not entropy/emacs-custom-enable-lazy-load)
-                (not entropy/emacs-fall-love-with-pdumper))
+          ((not entropy/emacs-custom-enable-lazy-load)
            (set hook (append (symbol-value hook) '(,func))))
           (t (defun ,adder-func ()
-               (dolist (item ,list-var)
+               (dolist (item ',list-var)
                  (if (not (null ,adder-flag))
                      (,adder-type item ,adder-flag ',func)
                    (,adder-type item ',func))))

elements/core/baron/startup/entropy-emacs-start.el
@@ -40,21 +40,12 @@
 (require 'entropy-emacs-defvar)
 (require 'entropy-emacs-message)
 
-(progn (when (and entropy/emacs-fall-love-with-pdumper
-                  entropy/emacs-custom-enable-lazy-load)
-         (setq entropy/emacs-custom-enable-lazy-load nil))
-
-       (when (and entropy/emacs-fall-love-with-pdumper
-                  entropy/emacs-enable-pyim)
-         (setq entropy/emacs-enable-pyim nil)
-         (entropy/emacs-message-do-message
-          (red "You can not enable pyim in pdumper procedure, but
-we inject it into pdumper session initialize procedure. "))
-         (add-hook 'entropy/emacs-pdumper-load-end-hook
-                   #'entropy/emacs-basic-pyim-start)))
-
-(when (daemonp)
-  (when entropy/emacs-custom-enable-lazy-load
+;; forbidden `entropy/emacs-custom-enable-lazy-load' at special
+;; session.
+(progn
+  (when (and (or entropy/emacs-fall-love-with-pdumper
+                 (daemonp))
+             entropy/emacs-custom-enable-lazy-load)
     (setq entropy/emacs-custom-enable-lazy-load nil)))
 
 ;; *** load core library
@@ -202,6 +193,8 @@ Emacs\" buffer's local `browse-url-browse-function' to
 and save the compiling log into `entropy/emacs-stuffs-topdir'
 named as 'compile_$date.log'."
   (let ((buflist (mapcar #'buffer-name (buffer-list))))
+    (when entropy/emacs-package-install-success-list
+      (setq entropy/emacs-start--is-init-with-install t))
     (when (member "*Compile-Log*" buflist)
       ;; First recorde compiling log
       (let (($f (expand-file-name (concat "compile_" (format-time-string "%Y-%m-%d_%a_%H%M%S") ".log")
@@ -214,101 +207,85 @@ named as 'compile_$date.log'."
           (goto-char (point-min))
           (insert buff_content)
           (save-buffer)
-          (kill-buffer)))
-      (setq entropy/emacs-start--is-init-with-install t)))
+          (kill-buffer)))))
   (defun entropy/emacs-start--check-init-with-install-p ()
     "This function has been unloaded."
     nil)
   (when entropy/emacs-start--is-init-with-install
     (run-with-timer 6 nil #'kill-emacs)))
 
-;; *** start type choice
-;; **** status of pyim loading
-(when entropy/emacs-enable-pyim
-  ;; for emacs 26 and higher version
-  (defvar entropy/emacs-start--pyim-timer-26+ nil)
-  (defvar entropy/emacs-start--pyim-init-done-26+ nil)
+(defun entropy/emacs-start--warn-with-pkg-install ()
+  (entropy/emacs-start--check-init-with-install-p)
+  (when entropy/emacs-start--is-init-with-install
+    (if (entropy/emacs-is-make-session)
+        (entropy/emacs-message-do-message
+         "%s"
+         (red "Remaining procedure can not loaded in this
+session because of you have installed some stuffs in this
+session, please restart thus, and it will be well."))
+      (warn "You init with installing new packages, please reopen emacs!
 
-  (defvar entropy/emacs-start--pyim-init-prompt-buffer "*pyim-cache-loading*")
+Emacs will auto close after 6s ......"))
+    (top-level)))
 
-  (defun entropy/emacs-start--pyim-init-after-loaded-cache-26+ ()
-    "Trace pyim loading thread til it's done as giving the
+;; *** status of pyim loading
+(defvar entropy/emacs-start--pyim-timer nil)
+(defvar entropy/emacs-start--pyim-init-done nil)
+
+(defvar entropy/emacs-start--pyim-init-prompt-buffer
+  (get-buffer-create "*pyim-cache-loading*"))
+
+(defun entropy/emacs-start--pyim-init-after-loaded-cache ()
+  "Trace pyim loading thread til it's done as giving the
 notation.
 
 (specific for emacs version uper than '26' or included '26'.)"
-    (when (< (length (all-threads)) 2)
-      (setq entropy/emacs-start--pyim-init-done-26+ t)
-      (when (bound-and-true-p entropy/emacs-start--pyim-timer-26+)
-        (cancel-function-timers #'entropy/emacs-start--pyim-init-after-loaded-cache-26+))
-      (when (buffer-live-p entropy/emacs-start--pyim-init-prompt-buffer)
-        (switch-to-buffer entropy/emacs-start--pyim-init-prompt-buffer)
-        (kill-buffer-and-window))
-      (entropy/emacs-message-do-message (green "pyim loading down."))
-      (defun entropy/emacs-start--pyim-init-after-loaded-cache-26+ ()
-        (entropy/emacs-message-do-message
-         (yellow "This function has been unloaded.")))))
-
-  ;; for emacs 25 and lower version
-  (defun entropy/emacs-start--pyim-init-after-loaded-cache-26- ()
-    "Giving the notation when loaded pyim cache.
-
-(specific for emacs version under '26')"
+  (when (< (length (all-threads)) 2)
+    (setq entropy/emacs-start--pyim-init-done t)
+    (when (bound-and-true-p entropy/emacs-start--pyim-timer)
+      (cancel-function-timers #'entropy/emacs-start--pyim-init-after-loaded-cache))
+    (when (buffer-live-p entropy/emacs-start--pyim-init-prompt-buffer)
+      (switch-to-buffer entropy/emacs-start--pyim-init-prompt-buffer)
+      (kill-buffer-and-window))
     (entropy/emacs-message-do-message (green "pyim loading down."))
-    (defun entropy/emacs-start--pyim-init-after-loaded-cache-26- ()
+    (defun entropy/emacs-start--pyim-init-after-loaded-cache ()
       (entropy/emacs-message-do-message
-       (yellow "This function has been unloaded."))))
+       (yellow "This function has been unloaded.")))))
 
-  (defun entropy/emacs-start--pyim-initialize ()
-    "Make prompt when loading and loded pyim cache for emacs init time.
+(defun entropy/emacs-start--pyim-initialize ()
+  "Make prompt when loading and loded pyim cache for emacs init time.
 
 For different emacs version using different tracing method:
-- for 26+: `entropy/emacs-start--pyim-init-after-loaded-cache-26+'
+- for 26+: `entropy/emacs-start--pyim-init-after-loaded-cache'
 - for 26-: `entropy/emacs-pyim-init-after-loaded-cache-26_'
 
 It's for that emacs version uper than 26 as pyim using thread for loading cache."
-    ;; preparation prompt loading pyim cache.
-    (cond
-     ((not (version< emacs-version "26"))
-      (let ((buffer (get-buffer-create entropy/emacs-start--pyim-init-prompt-buffer)))
-        (setq entropy/emacs-start--pyim-init-prompt-buffer buffer)
-        (with-current-buffer buffer
-          (insert (propertize "Loading pyim cache, please waiting ......" 'face 'warning)))
-        (split-window-vertically (- (window-total-height) 4))
-        (other-window 1)
-        (switch-to-buffer buffer)
-        (redisplay t)))
-     ((version< emacs-version "26")
-      (entropy/emacs-message-do-message
-       (blue (bold "Loading pyim cache ......")))))
+  ;; preparation prompt loading pyim cache.
+  (let ((buffer entropy/emacs-start--pyim-init-prompt-buffer))
+    (with-current-buffer buffer
+      (insert (propertize "Loading pyim cache, please waiting ......" 'face 'warning)))
+    (split-window-vertically (- (window-total-height) 4))
+    (other-window 1)
+    (switch-to-buffer buffer)
+    (redisplay t))
 
-    ;; initialize pyim
-    (entropy/emacs-basic-pyim-start)
+  ;; initialize pyim
+  (entropy/emacs-basic-pyim-start)
 
-    ;; prompt for loading pyim cache done.
-    (cond
-     ((not (version< emacs-version "26"))
-      (setq entropy/emacs-start--pyim-timer-26+
-            (run-with-idle-timer
-             1 200
-             #'entropy/emacs-start--pyim-init-after-loaded-cache-26+)))
-     ((version< emacs-version "26")
-      (entropy/emacs-start--pyim-init-after-loaded-cache-26-)))
-    ;; reset function
-    (defun entropy/emacs-start--pyim-initialize ()
-      (message "This function has been unloaded."))))
+  ;; prompt for loading pyim cache done.
+  (setq entropy/emacs-start--pyim-timer
+        (run-with-idle-timer
+         1 200
+         #'entropy/emacs-start--pyim-init-after-loaded-cache))
+  ;; reset function
+  (defun entropy/emacs-start--pyim-initialize ()
+    (message "This function has been unloaded.")))
 
 
-;; **** after load procedure
+;; *** after load procedure
 
 (defun entropy/emacs-start--init-after-load-initialze-process (enable aft-hook)
   (when enable
-    ;; check start with package install status
-    (when (not (entropy/emacs-is-make-session))
-      (entropy/emacs-start--check-init-with-install-p)
-      (when entropy/emacs-start--is-init-with-install
-        (warn "You init with installing new packages, please reopen emacs!
-
-Emacs will auto close after 6s ......")))
     ;; run after init hooks
     (unless entropy/emacs-start--is-init-with-install
       (entropy/emacs-message-do-message
@@ -319,10 +296,11 @@ Emacs will auto close after 6s ......")))
     ;; start pyim
     (when (and entropy/emacs-enable-pyim
                (not entropy/emacs-start--is-init-with-install))
-      (entropy/emacs-start--pyim-initialize))))
+      (add-hook 'entropy/emacs-startup-end-hook
+                #'entropy/emacs-start--pyim-initialize))))
 
-;; **** start up branch
-;; ***** mini start
+;; *** start up branch
+;; **** mini start
 (defun entropy/emacs-start-M-enable ()
   (entropy/emacs-message-do-message
    "%s %s"
@@ -331,8 +309,12 @@ Emacs will auto close after 6s ......")))
   (advice-add 'require :before #'entropy/emacs-start--require-loading)
   (advice-add 'redisplay :around #'entropy/emacs-start--initial-redisplay-advice)
 
-  ;; parse installed packages
+  ;; packages initializing
   (require 'entropy-emacs-package)
+  ;;; breaking remaining procedure while new package intalled within
+  ;;; this session, because some messy.
+  (add-hook 'entropy/emas-package-common-start-after-hook
+            #'entropy/emacs-start--warn-with-pkg-install)
   (entropy/emacs-package-common-start)
 
   ;; external depedencies scan and loading
@@ -383,7 +365,7 @@ Emacs will auto close after 6s ......")))
    (white "⮞")
    (green "Minimal start completed.")))
 
-;; ***** x enable
+;; **** x enable
 (defun entropy/emacs-start-X-enable ()
   (interactive)
   (advice-add 'require :before #'entropy/emacs-start--require-loading)
@@ -447,7 +429,7 @@ Emacs will auto close after 6s ......")))
    (white "⮞")
    (green "Full start completed.")))
 
-;; **** startup main function
+;; *** startup main function
 (defun entropy/emacs-start--init-X ()
   (entropy/emacs-start-M-enable)
   (entropy/emacs-start-X-enable))
@@ -478,10 +460,7 @@ Emacs will auto close after 6s ......")))
     (require 'entropy-emacs-path)
     (entropy/emacs-start--init-bingo)
     (unless entropy/emacs-fall-love-with-pdumper
-      (run-hooks 'entropy/emacs-startup-end-hook)
-      (entropy/emacs-echo-startup-done))
-    (unless entropy/emacs-fall-love-with-pdumper
-      (setq entropy/emacs-startup-done t))))
+      (entropy/emacs-run-startup-end-hook))))
 
 (if (or entropy/emacs-fall-love-with-pdumper
         (daemonp))

elements/core/tentacles/entropy-emacs-basic.el
@@ -610,11 +610,61 @@ Temp file was \"~/~entropy-artist.txt\""
     (setq cursor-type t)))
 
 ;; *** Global display line number mode
-(when (>= emacs-major-version 26)
-  (progn
-    (setq-default display-line-numbers-width-start t)
-    (when entropy/emacs-init-display-line-mode
-      (global-display-line-numbers-mode))))
+(defun entropy/emacs-basic--dspln-mode-around-advice
+    (orig-func &rest orig-args)
+  "Filters for `display-line-numbers-mode' to press it for some
+occasions. "
+  (unless (or (member major-mode
+                      '(vterm-mode
+                        shell-mode
+                        eshell-mode
+                        term-mode
+                        treemacs-mode
+                        neotree-mode
+                        dashboard-mode
+                        dired-mode
+                        eww-mode
+                        w3m-mode))
+              (bound-and-true-p entropy/emacs-ui-init-welcom-mode))
+    (apply orig-func orig-args)))
+
+(advice-add 'display-line-numbers-mode
+            :around
+            #'entropy/emacs-basic--dspln-mode-around-advice)
+
+(entropy/emacs-lazy-with-load-trail
+ global-display-line-numbers-mode
+ (setq-default display-line-numbers-width-start t)
+ (when entropy/emacs-init-display-line-numbers-mode
+   (global-display-line-numbers-mode)))
+
+;; *** Global hl-line mode
+(defun entropy/emacs-turn-on-hl-line-mode ()
+  "The filter for `hl-line-mode' available option in triggered
+buffer, in that case any conditions don't match the filter then
+`hl-line-mode' will be enabled."
+  (unless (or (member major-mode
+                      '(vterm-mode
+                        shell-mode
+                        eshell-mode
+                        term-mode
+                        dashboard-mode))
+              (bound-and-true-p entropy/emacs-ui-init-welcom-mode))
+    (hl-line-mode 1)))
+
+;; Build `hl-line-mode' based global mode, that it is different from
+;; `global-hl-line-mode' which use `global-hl-line-highlight-all' as
+;; the subroutine. So that we can toggle line highlight feature
+;; without global status restriction anti from what
+;; `global-hl-line-mode' did.
+(define-globalized-minor-mode entropy/emacs-hl-line-global-mode
+  hl-line-mode
+  entropy/emacs-turn-on-hl-line-mode)
+
+(entropy/emacs-lazy-with-load-trail
+ global-hl-line-mode
+ (when entropy/emacs-init-hl-line-mode
+   (entropy/emacs-hl-line-global-mode 1)))
 
 ;; *** Backup setting
 (setq-default auto-save-default nil)    ; disable it for preventing typing lagging
@@ -675,7 +725,7 @@ Filename are \".scratch_entropy\" host in
   (lisp-interaction-mode)
   (message "Create *scratch* buffer"))
 
-;; *** Highlight current line
+;; *** Highlight current line and display line numbers
 (defun entropy/emacs-basic--dhl-judge-state ()
   (let ((hlmp (ignore-errors hl-line-mode))
         (dlmp display-line-numbers)
@@ -694,31 +744,27 @@ Filename are \".scratch_entropy\" host in
 
 (defun entropy/emacs-basic-dhl-toggle ($Prefix)
   (interactive "P")
-  (if (not (version< emacs-version "26.1"))
-      (if (not $Prefix)
-          (cl-case (entropy/emacs-basic--dhl-judge-state)
-            (1
-             (hl-line-mode 1))
-            (2
-             (hl-line-mode 1))
-            (3
-             (hl-line-mode 0))
-            (4
-             (hl-line-mode 0)))
-        (cl-case (entropy/emacs-basic--dhl-judge-state)
-          (1
-           (hl-line-mode 1)
-           (display-line-numbers-mode 1))
-          (2
-           (hl-line-mode 1))
-          (3
-           (display-line-numbers-mode 1))
-          (4
-           (hl-line-mode 0)
-           (display-line-numbers-mode 0))))
-    (if (not (ignore-errors hl-line-mode))
-        (hl-line-mode 1)
-      (hl-line-mode 0))))
+  (if (not $Prefix)
+      (cl-case (entropy/emacs-basic--dhl-judge-state)
+        (1
+         (hl-line-mode 1))
+        (2
+         (hl-line-mode 1))
+        (3
+         (hl-line-mode 0))
+        (4
+         (hl-line-mode 0)))
+    (cl-case (entropy/emacs-basic--dhl-judge-state)
+      (1
+       (hl-line-mode 1)
+       (display-line-numbers-mode 1))
+      (2
+       (hl-line-mode 1))
+      (3
+       (display-line-numbers-mode 1))
+      (4
+       (hl-line-mode 0)
+       (display-line-numbers-mode 0)))))
 
 ;; *** Smooth scrolling
 
@@ -1022,7 +1068,7 @@ This affected by `neotree' or `treemacs' window sticking with
   :commands (entropy-grom-mode)
   :init
   (entropy/emacs-lazy-initial-advice-before
-   '(find-file push-button find-library-name)
+   (find-file push-button find-library-name)
    "entropy-grom"
    "entropy-grom"
    (entropy-grom-mode +1))
@@ -1033,7 +1079,7 @@ This affected by `neotree' or `treemacs' window sticking with
 ;; *** Revert buffer automatically
 
 (entropy/emacs-lazy-initial-for-hook
- '(find-file-hook)
+ (find-file-hook)
  "GlbAutoRevertMode"
  "GlbAutoRevertMode-enabled"
  (global-auto-revert-mode +1))
@@ -1587,7 +1633,7 @@ See [[https://github.com/rime/home/wiki/CustomizationGuide#%E4%B8%80%E4%BE%8B%E5
   :ensure nil
   :init
   (entropy/emacs-lazy-initial-for-hook
-   '(dired-mode-hook find-file-hook)
+   (dired-mode-hook find-file-hook)
    "epa-mode" "epa-mode"
    (epa-file-enable))
 
@@ -1780,7 +1826,7 @@ otherwise returns nil."
                      (auto-compression-mode 1))))
       (t
        (entropy/emacs-lazy-initial-advice-before
-        '(push-button load-library find-library)
+        (push-button load-library find-library)
         "autocompression-mode"
         "autocompression-mode"
         (auto-compression-mode 0)

elements/core/tentacles/entropy-emacs-calendar.el
@@ -44,7 +44,7 @@
   :commands cal-china-x-setup
   :init
   (entropy/emacs-lazy-initial-for-hook
-   '(calendar-mode-hook)
+   (calendar-mode-hook)
    "calendar-chinaX-mode"
    "calendar-chinaX-mode"
    (cal-china-x-setup))

elements/core/tentacles/entropy-emacs-codeserver.el
@@ -223,7 +223,7 @@ nervous."
        #'lsp-deferred)))
 
   (entropy/emacs-lazy-initial-advice-before
-   '(lsp)
+   (lsp)
    "lsp-enable-yas"
    "lsp-enable-yas"
    (require 'yasnippet)

elements/core/tentacles/entropy-emacs-company.el
@@ -265,7 +265,7 @@
               entropy/emacs-company-idle-delay-default))
       (entropy/emacs-message-do-message
        "%s '%s' to '%s'"
-       (blue "set")
+       (blue "Set")
        (yellow (symbol-name 'company-idle-delay))
        (red (number-to-string company-idle-delay)))))
 

elements/core/tentacles/entropy-emacs-ivy.el
@@ -543,7 +543,7 @@ this variable used to patching for origin `counsel-git'.")
               #'entropy/emacs-ivy--enable-ivy-rich-common))
    (t
     (entropy/emacs-lazy-initial-advice-before
-     '(ivy-read)
+     (ivy-read)
      "ivy-rich" "ivy-rich"
      (entropy/emacs-ivy--enable-ivy-rich-common))))
 

elements/core/tentacles/entropy-emacs-org.el
@@ -180,7 +180,7 @@
          'org-babel-load-languages ob-lang))))
 
   (entropy/emacs-lazy-initial-advice-before
-   '(org-mode)
+   (org-mode)
    "org-mode"
    "org-mode"
    (when (and (not entropy/emacs-fall-love-with-pdumper)

commit 2d6b7638779a095798a4b02ff7d09600dd8981ee

Fix neglect caused by commit b1b5e9ec

elements/core/wasteland/func-binds/entropy-emacs-defun.el
@@ -7315,7 +7315,7 @@ lexical environment."
                           ,body-lambda-exp-sym)))
              ((and (listp ,feature-sym)
                    (= 1 (length ,feature-sym)))
-              (setq forms
+              (setq ,forms-sym
                     (list 'eval-after-load (funcall ,extitem-func-sym (car ,feature-sym))
                           ,body-lambda-exp-sym)))
              ((and (listp ,feature-sym)