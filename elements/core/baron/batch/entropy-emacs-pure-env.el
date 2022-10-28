5dc0c02bb930a7b44e18dff06eeb370b0511f4d8
Entroperiance.J <bmsac0001@gmail.com>
2022-10-29 03:07:35 +0800

    Remove obsolete

--- elements/core/baron/batch/entropy-emacs-pure-env.el
+++ elements/core/baron/batch/entropy-emacs-pure-env.el
@@ -1,15 +1,3 @@
-;; EEMACS_MAINTENANCE: we must forbidden native-comp for polluting
-;; eemacs current usage status i.e eemacs usually has its specified
-;; native-comp mechanism.
-(when (featurep 'comp)
-  (setq native-comp-always-compile nil
-        native-comp-deferred-compilation nil)
-  (setq native-comp-deferred-compilation-deny-list
-        ;; we must excluded eemacs code for native comp
-        '("entropy-emacs-.*"
-          "liberime*"
-          "fakecygpty"
-          "\\.?dir-locals")))
 ;; load the core subroutine
 (entropy/emacs-common-require-feature 'entropy-emacs-defun)
 (provide 'entropy-emacs-pure-env)
