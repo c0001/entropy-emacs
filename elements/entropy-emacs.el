87ec53d5550af07252b8e7c80f3fb7d4ffca20bb
Entroperiance.J <bmsac0001@gmail.com>
2022-11-13 18:19:37 +0800

    Retype

--- elements/entropy-emacs.el
+++ elements/entropy-emacs.el
@@ -187,8 +187,7 @@ and cond2 are both of non-nil or nil."
 This function exists since user usually use `get' for
 `this-command' which is not always a symbol as it declared that
 its value is a COMMAND."
-  (if (symbolp maybe-sym)
-      (get maybe-sym prop)))
+  (if (symbolp maybe-sym) (get maybe-sym prop)))
 
 (defun entropy/emacs-child-frame-p (&optional frame)
   "Return FRAME's parent frame if it is a child-frame (See Info node
