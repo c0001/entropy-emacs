41b3a567286b5323bb1e429cfa60f2109a3bb4fb
Entroperiance.J <bmsac0001@gmail.com>
2023-04-09 17:17:18 +0800

    Do not dups comp clean all while EEMACS_MAKE_ALL

--- elements/core/baron/batch/entropy-emacs-batch.el
+++ elements/core/baron/batch/entropy-emacs-batch.el
@@ -816,7 +816,8 @@ since we solved deps broken")))))
       ;; we must check all depedencies firstly while compile
       (entropy/emacs-batch--check-packages)
       (entropy/emacs-batch--prompts-for-byte-compile-eemacs-internal
-       (entropy/emacs-batch--do-bytecompile-eemacs-core t)
+       (unless (entropy/emacs-getenv "EEMACS_MAKE_ALL")
+         (entropy/emacs-batch--do-bytecompile-eemacs-core t))
        (entropy/emacs-batch--do-bytecompile-eemacs-core)))
      ((equal type "Compile-Clean")
       (entropy/emacs-batch--prompts-for-byte-compile-eemacs-internal
