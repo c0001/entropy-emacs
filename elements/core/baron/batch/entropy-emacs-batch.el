f766b15a807ed22ae8f4a6aea53c23d93149072b
Entroperiance.J <bmsac0001@gmail.com>
2023-04-09 14:53:06 +0800

    [Makefile] Always clean before compile

--- elements/core/baron/batch/entropy-emacs-batch.el
+++ elements/core/baron/batch/entropy-emacs-batch.el
@@ -816,6 +816,7 @@ since we solved deps broken")))))
       ;; we must check all depedencies firstly while compile
       (entropy/emacs-batch--check-packages)
       (entropy/emacs-batch--prompts-for-byte-compile-eemacs-internal
+       (entropy/emacs-batch--do-bytecompile-eemacs-core t)
        (entropy/emacs-batch--do-bytecompile-eemacs-core)))
      ((equal type "Compile-Clean")
       (entropy/emacs-batch--prompts-for-byte-compile-eemacs-internal
