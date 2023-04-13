c0001/entropy-emacs

commit 59e08b116fe9f25a9869a5b630f61dec171181e2

Daemon set of `xclip-mode'

elements/core/wasteland/func-binds/entropy-emacs-defun.el
@@ -12669,7 +12669,15 @@ clipboard with native operation system."
                                               ;; executable
                                               (executable-find "powershell.exe")
                                             (symbol-name judger)))
-                      (xclip-mode 1)
+                      (entropy/emacs-run-body-only-once
+                       (entropy/emacs-with-daemon-make-frame-done
+                         'eemacs-set-xclip-mode (&rest _)
+                         :when-tui (xclip-mode 1)
+                         :when-gui (xclip-mode 0)
+                         (when (and (not noninteractive)
+                                    (not (daemonp))
+                                    (not (display-graphic-p)))
+                           (xclip-mode 1))))
                       t))))
       judger)))
 

commit f7b164e34bf714672d0d3adecd8863b886991181

Fix neglect caused by 59e08b11

elements/core/wasteland/func-binds/entropy-emacs-defun.el
@@ -12680,11 +12680,12 @@ clipboard with native operation system."
                        (entropy/emacs-with-daemon-make-frame-done
                          'eemacs-set-xclip-mode (&rest _)
                          :when-tui (xclip-mode 1)
-                         :when-gui (xclip-mode 0)
-                         (when (and (not noninteractive)
-                                    (not (daemonp))
-                                    (not (display-graphic-p)))
-                           (xclip-mode 1))))
+                         :when-gui (xclip-mode 0)))
+                      (when (and (not noninteractive)
+                                 (not (daemonp))
+                                 (not (display-graphic-p))
+                                 (not (bound-and-true-p xclip-mode)))
+                        (xclip-mode 1))
                       t))))
       judger)))
 