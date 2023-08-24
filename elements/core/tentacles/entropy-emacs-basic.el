ee85147d2d528f953730b20c040f6078969ff5d1
Entroperiance.J <bmsac0001@gmail.com>
2023-08-24 22:18:19 +0800

    Going on ...

--- elements/core/tentacles/entropy-emacs-basic.el
+++ elements/core/tentacles/entropy-emacs-basic.el
@@ -364,13 +364,17 @@ With prefix argument binds, jump to the previous mark place."
     "This advice let `dired-revert' takes care of the user nervousness
 and result feedback for a refresh of the result of the directory
 user expected to known about."
-    (entropy/emacs-message-simple-progress-message
-        (let ((cur-buff (current-buffer))
-              (fmstr "Reverting dired buffer %S"))
-          (when (bound-and-true-p dired-omit-mode)
-            (setq fmstr (concat fmstr " (with omitting nodes)")))
-          (format fmstr cur-buff))
-      (apply orig-func orig-args)))
+    (if (not (eq this-command 'revert-buffer))
+        ;; only for diretly interactiely invoking `revert-buffer' so
+        ;; that doesn't pollute msg from other commands.
+        (apply orig-func orig-args)
+      (entropy/emacs-message-simple-progress-message
+          (let ((cur-buff (current-buffer))
+                (fmstr "Reverting dired buffer %S"))
+            (when (bound-and-true-p dired-omit-mode)
+              (setq fmstr (concat fmstr " (with omitting nodes)")))
+            (format fmstr cur-buff))
+        (apply orig-func orig-args))))
   (advice-add 'dired-revert
               :around
               #'__ya/dired-revert/with-prompting)
