c0001/entropy-emacs

commit 48378be6761df08378cbfeb1b5d028b2644c7d14

[image-dired] fix db search unsafety regexp

elements/core/tentacles/entropy-emacs-basic.el
@@ -3826,6 +3826,25 @@ displayed image as same operated mechanism as
 ;; ****** __end__
   )
 
+;; ***** others
+;; ****** patch no safety image-dired db regexp search
+(defun entropy/emacs-basic--patch-image-dired-db-search-regexp nil
+  (advice-patch
+   'image-dired-list-tags
+   '(search-forward-regexp (format "^%s" (regexp-quote file)) nil t)
+   '(search-forward-regexp (format "^%s" file) nil t))
+  (advice-patch
+   'image-dired-get-comment
+   '(search-forward-regexp (format "^%s" (regexp-quote file)) nil t)
+   '(search-forward-regexp (format "^%s" file) nil t)))
+
+(cond ((< emacs-major-version 29)
+       (with-eval-after-load 'image-dired
+         (entropy/emacs-basic--patch-image-dired-db-search-regexp)))
+      (t
+       (with-eval-after-load 'image-dired-tags
+         (entropy/emacs-basic--patch-image-dired-db-search-regexp))))
+
 ;; *** Artist-mode
 (use-package artist
   :ensure nil

commit 0edb784cdb4785d8937723e24be5ab5564f05635

Fix CI regression from commit: 48378be6

elements/core/tentacles/entropy-emacs-basic.el
@@ -3838,10 +3838,13 @@ displayed image as same operated mechanism as
    '(search-forward-regexp (format "^%s" (regexp-quote file)) nil t)
    '(search-forward-regexp (format "^%s" file) nil t)))
 
-(cond ((< emacs-major-version 29)
+(cond (
+       ;; since we use self-maintained image-28 dired ver. which has
+       ;; patched yet.
+       (< emacs-major-version 28)
        (with-eval-after-load 'image-dired
          (entropy/emacs-basic--patch-image-dired-db-search-regexp)))
-      (t
+      ((> emacs-major-version 28)
        (with-eval-after-load 'image-dired-tags
          (entropy/emacs-basic--patch-image-dired-db-search-regexp))))
 