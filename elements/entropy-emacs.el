c73a2de95b31649f528b1059e1f45a2757bb5edf
Entroperiance.J <bmsac0001@gmail.com>
2025-04-14 16:40:23 +0800

    going on

--- elements/entropy-emacs.el
+++ elements/entropy-emacs.el
@@ -1554,7 +1554,7 @@ byte-compile generated but source loading undeeded.")
           "undo-tree-eemacs"
           "liberime"
           "emacs-rime"
-          "eemacs-treemacs"
+          "eemacs-treemacs/src/elisp"
           "with-proxy.el"
           "lsp-java-simple"))
        (cur-path (expand-file-name (file-name-directory load-file-name)))
