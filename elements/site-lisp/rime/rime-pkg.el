2979a4a3f0a8d4709c20a7273e764eff0e755abc
Entroperiance.J <bmsac0001@gmail.com>
2025-04-15 14:25:22 +0800

    fetch-3rds: add third party pkg -- rime
    from:   https://github.com/c0001/emacs-rime.git
    commit: 4be1cf2

---
+++ elements/site-lisp/rime/rime-pkg.el
@@ -0,0 +1,6 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "rime" "20241211.160836"
+  "Rime input method for emacs"
+  '((emacs "26.3"))
+  :url "https://github.com/c0001/emacd-rime"
+  :keywords '("convenience" "input-method"))
