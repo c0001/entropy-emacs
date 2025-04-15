bbe75498e7a9ae28a546b49ee099f30b1709727e
Entroperiance.J <bmsac0001@gmail.com>
2025-04-15 14:25:21 +0800

    fetch-3rds: add third party pkg -- treemacs
    from:   https://github.com/c0001/treemacs.git
    commit: b44716a

---
+++ elements/site-lisp/treemacs/treemacs-pkg.el
@@ -0,0 +1,6 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "treemacs" "3.2"
+  "A tree style file explorer package"
+  '((emacs "26.1") (cl-lib "0.5") (dash "2.11.0") (s "1.12.0") (ace-window "0.9.0") (pfuture "1.7") (hydra "0.13.2") (ht "2.2") (cfrs "1.3.2"))
+  :url "https://github.com/c0001/treemacs"
+  :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")))
