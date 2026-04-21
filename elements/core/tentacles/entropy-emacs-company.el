7730c7dabd3a651353b0b24b59d27a23cec9c9ce
Entroperiance.J <bmsac0001@gmail.com>
2026-04-22 07:35:27 +0800

    fix

--- elements/core/tentacles/entropy-emacs-company.el
+++ elements/core/tentacles/entropy-emacs-company.el
@@ -359,6 +359,14 @@ sessions performance."
   (setq company-emulation-alist
         (entropy/emacs-double-list 'company-candidates))
 
+  ;; FIXME: emacs-31 made `all' as a function so that the `all'
+  ;; indicator will cause error for `company-dabbrev--fetch' function
+  ;; pred.
+  (when (and (version< "30" emacs-version)
+             (eq company-dabbrev-other-buffers 'all))
+    (setq company-dabbrev-other-buffers
+          (lambda (&rest _) nil)))
+
 ;; **** advices
 ;; ***** `company-perform' performance adjusting
 
